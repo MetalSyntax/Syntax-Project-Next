dcd06b85340e187cad92e2d13d554c5c53fc55cc
Wonder <wonderdxk@gmail.com>
2020-05-09 02:17:58 -0400

    1.0.1
    Create cards
    Create Footer and Copyrighy
    Fix Title of Hero
    Others Fix

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default () => (
-  <div className="">
+  <div id="home">
     <nav className="flex items-center justify-between flex-wrap nav-bg p-4">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <img className="h-16 w-16 rounded-full" src="/TechCube.png" alt="Tech Cube" />
+        <img className="h-16 w-16 rounded-full border-solid border-2 border-white" src="/TechCube.png" alt="Tech Cube" />
       </div>
       <div className="block lg:hidden">
         <button className="flex items-center px-3 py-2 rounded text-purple-200 ">
@@ -26,47 +26,67 @@ export default () => (
         </div>
       </div>
     </nav>
-    <div id="hero" className="flex flex-wrap content-center py-8 px-4 h-screen">
+    <section id="hero" className="flex flex-wrap content-center py-8 px-4 h-screen">
       <div className="w-full">
-        <h1 className="text-purple-100 text-3xl text-center items-center">
+        <h1 className="text-purple-100 text-4xl md:text-5xl lg:text-6xl text-center items-center">
           Tech Cube
         </h1>
       </div>
-    </div>
-    <div className="flex flex-wrap -mx-2 py-4 px-2">
-    <div className="sm:w-full md:w-1/2 lg:w-1/3 px-4 py-2">
-      <div class="rounded overflow-hidden shadow-lg my-2 px-4 bg-card">
-        <div class="px-4 py-4">
-          <div className="font-bold text-xl mb-2 text-center">
-          <button class="bt-color bd-color font-bold rounded">
-            Button
+    </section>
+    <section id="cards" className="flex flex-wrap py-6 px-2">
+    <div className="sm:w-full lg:w-1/3 px-4 py-2">
+      <div className="rounded overflow-hidden shadow-lg my-2 bg-card">
+      <img class="w-full" src="/CardsX3.jpg" alt=""/>
+        <div className="px-4 py-4">
+          <div className="font-bold text-xl text-center">
+          <button className="bt-color bd-color font-bold rounded py-1 px-10">
+            Learn More
           </button>
           </div>
         </div>
       </div>
     </div>
-    <div className="sm:w-full md:w-1/2 lg:w-1/3 px-4 py-2">
-      <div class="rounded overflow-hidden shadow-lg my-2 px-4 bg-card">
-        <div class="px-4 py-4">
-          <div className="font-bold text-xl mb-2 text-center">
-          <button class="bt-color bd-color font-bold rounded">
-            Button
+    <div className="sm:w-full lg:w-1/3 px-4 py-2">
+      <div className="rounded overflow-hidden shadow-lg my-2 bg-card">
+      <img class="w-full" src="/Cards2X3.jpg" alt=""/>
+        <div className="px-4 py-4">
+          <div className="font-bold text-xl text-center">
+          <button className="bt-color bd-color font-bold rounded py-1 px-10">
+            Learn More
           </button>
           </div>
         </div>
       </div>
     </div>
-    <div className="sm:w-full md:w-1/2 lg:w-1/3 px-4 py-2">
-      <div class="rounded overflow-hidden shadow-lg my-2 px-4 bg-card">
-        <div class="px-4 py-4">
-        <div className="font-bold text-xl mb-2 text-center">
-          <button class="bt-color bd-color font-bold rounded">
-            Button
+    <div className="sm:w-full lg:w-1/3 px-4 py-2">
+      <div className="rounded overflow-hidden shadow-lg my-2 bg-card">
+      <img class="w-full" src="/Cards3X3.jpg" alt=""/>
+        <div className="px-4 py-4">
+        <div className="font-bold text-xl text-center">
+          <button className="bt-color bd-color font-bold rounded py-1 px-10">
+            Learn More
           </button>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
+  <footer>
+    <div class="flex flex-wrap">
+      <div class="w-full bt-color py-2">
+        <img className="h-16 w-16 rounded-full border-solid border-2 border-white m-auto" src="/TechCube.png" alt="Tech Cube" />
+        <p className="text-purple-100 text-2xl text-center items-center">
+          Tech Cube
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-wrap">
+      <div class="w-full cr-color py-2">
+        <p className="text-purple-100 text-1xl text-center items-center">
+        Develop by MetalSyntax. 2020
+        </p>
+      </div>
+    </div>
+  </footer>
 </div>
 );
